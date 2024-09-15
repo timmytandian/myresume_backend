46bd2ec7ba1a0c94875e5674bd2862d4262a3ab0
pop timmytandian <m37656192+timmytandian@users.noreply.github.com>
2024-09-15 16:37:04 +0900

    remove the block code to initialize dynamodb_table_item resource. Reason: in production environment, I do not initialize DynamoDB, instead I import an existing one.

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -1,12 +1,3 @@
-# NOTE: the configuration of random_uuid and 2 aws_dynamodb_table_item are applied only once,
-# only applied when we initialize the Dynamo DB table. After the initialization finished, 
-# we should remove them from the terraform state file. Command:
-#   terraform state list // to list all resources managed by the state file
-#   terraform state rm RESOURCE.ADDRESS // to remove the target resource
-# After the removal finished, we should track the pkey_uuid as data resource (as output)
-# so its value can be used in other modules.
-
-
 # DYNAMO DB TABLE
 resource "aws_dynamodb_table" "visitors_count_db" {
   name                        = "${var.dynamodb_table_name}${var.env == "prod" ? "" : "_${var.env}"}"
@@ -47,43 +38,3 @@ locals {
   page_name = try(local.item_data.page_name.S, "")
   visit_count = try(local.item_data.visit_count.N, 0)
 }
-
-
-/*
-# NOTE: As mentioned above, the configuration of 3 resources below
-# should be applied only once as initialization. After the initialization finished, 
-# we should remove them from the terraform state file.
-
-# RESOURCE #1: Random UUID
-resource "random_uuid" "home_pkey_uuid" {
-
-}
-
-# RESOURCE #2: The table item that represent the visitor count
-resource "aws_dynamodb_table_item" "visit_count" {
-  table_name = aws_dynamodb_table.visitors_count_db.name
-  hash_key   = aws_dynamodb_table.visitors_count_db.hash_key
-
-  item = <<ITEM
-{
-  "pkey_uuid": {"S": "${random_uuid.home_pkey_uuid.result}"},
-  "visit_count": {"N": "0"},
-  "page_name": {"S": "home"}
-}
-ITEM
-
-  depends_on = [random_uuid.home_pkey_uuid]
-}
-
-# RESOURCE #3: Table item to make sure that "home" value in "page_name" column is unique
-resource "aws_dynamodb_table_item" "unique_home_page_name" {
-  table_name = aws_dynamodb_table.visitors_count_db.name
-  hash_key   = aws_dynamodb_table.visitors_count_db.hash_key
-
-  item = <<ITEM
-{
-  "pkey_uuid": {"S": "page_name#home"}
-}
-ITEM
-
-}*/
