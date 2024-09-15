83904e19ab32479a16fafe97ebd92f8a15c20725
pop timmytandian <m37656192+timmytandian@users.noreply.github.com>
2024-09-15 16:31:15 +0900

    Comment the import block.

--- terraform/environments/prod/import_dynamodb.tf
+++ terraform/environments/prod/import_dynamodb.tf
@@ -1,7 +1,8 @@
 # Use the block below to import the resource from AWS
 # IMPORTANT: after the import procedure finished, the code below should be commented out.
 
+/*
 import {
   to = module.dynamodb.aws_dynamodb_table.visitors_count_db
   id = "cloud_resume"
-}
+}*/
