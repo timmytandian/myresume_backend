d3052e8141a16f47b2cf36862b34d5bfea791682
pop timmytandian <m37656192+timmytandian@users.noreply.github.com>
2024-09-17 02:17:13 +0900

    fix: the lambda_function_arn output was referring to the lambda_function_name output. Fixed this.

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -24,6 +24,6 @@ output "lambda_function_name" {
 }
 
 output "lambda_function_arn" {
-  value       = module.lambda.lambda_function_name
+  value       = module.lambda.lambda_function_arn
   description = "The name of Lambda function that can interact with DynamoDB table."
 }
